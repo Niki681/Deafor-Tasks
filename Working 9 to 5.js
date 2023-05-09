7c6172be4821ec3b61043a9c01e035ed86476001
Niki681 <nkatarski@gmail.com>
2023-05-09 21:13:50 +0300

    DeaforInh

--- Working 9 to 5.js
+++ Working 9 to 5.js
@@ -12,14 +12,14 @@
 // - $ + earned that day (rounded to the nearest hundredth)
 
 
-function calculatePay(arr) {
-    const start = arr[0]; // start day
-    const end = arr[1]; // end day
+function dayPayment(arr) {
+    const startDay = arr[0]; // start day
+    const endDay = arr[1]; // end day
     const rate = arr[2]; // hourly rate
     const multiplier = arr[3]; // overtime multiplier
   
     let totalPay = 0;
-    for (let i = start; i < end; i++) {
+    for (let i = startDay; i < endDay; i++) {
       if (i < 17) {
         totalPay += rate;
       } else {
@@ -30,7 +30,7 @@ function calculatePay(arr) {
     return totalPay;
     
   }
-  console.log(calculatePay([9, 17, 30, 1.5]));
-  console.log(calculatePay([16, 18, 30, 1.8]));
-  console.log(calculatePay([13.25, 15, 30, 1.5]));
-  console.log(calculatePay([10, 14, 10, 1]));
+  console.log(dayPayment([9, 17, 30, 1.5]));
+  console.log(dayPayment([16, 18, 30, 1.8]));
+  console.log(dayPayment([13.25, 15, 30, 1.5]));
+  console.log(dayPayment([10, 14, 10, 1]));
